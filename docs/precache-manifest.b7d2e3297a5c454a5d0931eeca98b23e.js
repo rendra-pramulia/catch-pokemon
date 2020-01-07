96c1d96e508a644c1c5488cd5dd02a20fa8f67e3
rendrapramulia <rendra.pramulia@alfadigital.id>
2020-01-07 10:23:26 +0700

    create-react-app build

---
+++ docs/precache-manifest.b7d2e3297a5c454a5d0931eeca98b23e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "507784424b920987d2aa8623b6c7f5c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "334509b8618fa755cac3",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "83e812e3edf07b545474",
+    "url": "/static/js/2.c1465ba8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c1465ba8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "334509b8618fa755cac3",
+    "url": "/static/js/main.dfe68cc3.chunk.js"
+  },
+  {
+    "revision": "464eb20ee443606940f9",
+    "url": "/static/js/runtime-main.46f2c0ba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
